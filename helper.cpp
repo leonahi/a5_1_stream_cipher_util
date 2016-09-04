9f08f23af5a59386f8bbda0e1cc4f4fbce45deac
Nahit Pawar <nahitpawar@gmail.com>
2016-09-05 02:46:53 +0530

    added new function in helper.cpp - a5_init(...)

--- helper.cpp
+++ helper.cpp
@@ -24,9 +24,10 @@ void shift_left_lfsr(unsigned long &reg, unsigned long mask, unsigned long taps)
     reg |= parity(shifted_bit);
 }
 
+// Clock all LFSRs conditionaly
 void conditional_shift_left_lfsr(unsigned long &LFSR1, unsigned long &LFSR2, unsigned long &LFSR3)
 {
-    bool m=majority(LFSR1, LFSR2, LFSR3);
+    register bool m=majority(LFSR1, LFSR2, LFSR3);
     if (((LFSR1 & LFSR1_CLK_BIT)!=0) == m) 
         shift_left_lfsr(LFSR1, LFSR1_BITMASK, LFSR1_TAP_BITMASK);
     if (((LFSR2 & LFSR2_CLK_BIT)!=0) == m)
@@ -35,9 +36,67 @@ void conditional_shift_left_lfsr(unsigned long &LFSR1, unsigned long &LFSR2, uns
         shift_left_lfsr(LFSR3, LFSR3_BITMASK, LFSR3_TAP_BITMASK);   
 }
 
+// Clock all LFSRs without condition
 void clock_all(unsigned long &LFSR1, unsigned long &LFSR2, unsigned long &LFSR3)
 {
     shift_left_lfsr(LFSR1, LFSR1_BITMASK, LFSR1_TAP_BITMASK);
     shift_left_lfsr(LFSR2, LFSR2_BITMASK, LFSR2_TAP_BITMASK);
     shift_left_lfsr(LFSR3, LFSR3_BITMASK, LFSR3_TAP_BITMASK);
+}
+
+// Initialization of A5/1 LFSRs
+void a5_init(char *key, unsigned long frame_no, unsigned long &LFSR1, unsigned long &LFSR2, unsigned long &LFSR3)
+{
+    register bool bit;
+    
+    // Initialize LFSRs to zero
+    LFSR1 = 0;
+    LFSR2 = 0;
+    LFSR3 = 0;
+    
+    // A5/1 keysetup
+    for(register int i=0; i<8; ++i)
+    {
+        for(register int j=0; j<8; ++j)
+        {
+            clock_all(LFSR1, LFSR2, LFSR3);
+            bit = (key[i] >> j) & 1;
+            LFSR1 ^= bit;
+            LFSR2 ^= bit;
+            LFSR3 ^= bit;
+        }
+    }
+    
+    // Load frame number 
+    for(register int i=0; i<22; ++i)
+    {
+        clock_all(LFSR1, LFSR2, LFSR3);
+        bit = ((frame_no >> i) & 1);
+        LFSR1 ^= bit;
+        LFSR2 ^= bit;
+        LFSR3 ^= bit;
+    }
+    
+    // Clock for 100 cycle for key mixing
+    for(register int i=0; i<100; ++i)
+        conditional_shift_left_lfsr(LFSR1, LFSR2, LFSR3);
+        
+}
+
+bool get_a5_key(unsigned long LFSR1, unsigned long LFSR2, unsigned long LFSR3)
+{
+    return parity(LFSR1 & LFSR1_OUTPUT_BIT) ^ parity(LFSR2 & LFSR2_OUTPUT_BIT) ^ parity(LFSR3 & LFSR3_OUTPUT_BIT);
+}
+
+void generate_keystream(unsigned short &keystream, unsigned long LFSR1, unsigned long LFSR2, unsigned long LFSR3)
+{
+    register bool bit;
+    keystream = 0;
+    for(register int i=0; i<16; ++i)
+    {
+        conditional_shift_left_lfsr(LFSR1, LFSR2, LFSR3);
+        bit = get_a5_key(LFSR1, LFSR2, LFSR3);
+        keystream = ((keystream << 1)|bit);
+        cout << bit << " : "<<  bitset<16>(keystream) << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@ extern bool parity(unsigned long reg);
 extern void shift_left_lfsr(unsigned long &reg, unsigned long mask, unsigned long taps);
 extern bool majority(unsigned long LFSR1, unsigned long LFSR2, unsigned long LFSR3);
 extern void conditional_shift_left_lfsr(unsigned long &LFSR1, unsigned long &LFSR2, unsigned long &LFSR3);
+extern void a5_init(char *key, unsigned long frame, unsigned long &LFSR1, unsigned long &LFSR2, unsigned long &LFSR3);
+extern void generate_keystream(unsigned short &keystream, unsigned long LFSR1, unsigned long LFSR2, unsigned long LFSR3);
 
 int main(int argc, char* argv[])
 {
     unsigned long LFSR1, LFSR2, LFSR3;
+    char key[8] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    unsigned long frame_no = 22;
+    unsigned short keystream=0;
     
     cout << parity(7) << endl;
     
@@ -22,7 +27,7 @@ int main(int argc, char* argv[])
     shift_left_lfsr(LFSR1, LFSR1_BITMASK, LFSR1_TAP_BITMASK);
     cout << "LFSR1 : " << bitset<32>(LFSR1) << endl;
     */
-    
+    /*
     cout << endl;
     cout << "LFSR1 : " << bitset<32>(LFSR1 & LFSR1_BITMASK) << endl;
     cout << "LFSR2 : " << bitset<32>(LFSR2 & LFSR2_BITMASK) << endl;
@@ -34,7 +39,17 @@ int main(int argc, char* argv[])
     cout << "LFSR1 : " << bitset<32>(LFSR1) << endl;
     cout << "LFSR2 : " << bitset<32>(LFSR2) << endl;
     cout << "LFSR3 : " << bitset<32>(LFSR3) << endl;
+    */
+    cout << "key : " << bitset<8>(key[0]) << endl;
+    cout << "key : " << bitset<8>((key[0]<<1)) << endl;
     
+    a5_init(key, frame_no, LFSR1, LFSR2, LFSR3);
+    cout << "LFSR1 : " << bitset<20>(LFSR1 & LFSR1_BITMASK) << endl;
+    cout << "LFSR2 : " << bitset<20>(LFSR1 & LFSR1_BITMASK) << endl;
+    cout << "LFSR3 : " << bitset<20>(LFSR1 & LFSR1_BITMASK) << endl;
+    
+    generate_keystream(keystream, LFSR1, LFSR2, LFSR3);
+    cout << "keystream : " << bitset<16>(keystream) << endl;
     
     
     return 0;
